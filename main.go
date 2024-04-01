b07fd47d92d2d17b57a1cbdb197a626d88467931
Jacob Jensen <jacob.jensen@instacart.com>
2024-04-01 09:40:02 -0400

    stub lexer test

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 	"monkey/lexer"
-	"monkey/tokens"
 )
 
 func main() {
 	fmt.Println(lexer.PrintHello())
-	fmt.Println(tokens.PrintWorld())
+	fmt.Println(token.PrintWorld())
 }
--- tokens/tokens.go
+++
@@ -1,5 +0,0 @@
-package tokens
-
-func PrintWorld() string {
-	return "World!"
-}

21b04e638aae282d245407b978af57a6e8680254
Jacob Jensen <jacob.jensen@instacart.com>
2024-04-05 11:28:23 -0400

    parse Identifiers (through p72)

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -141,3 +141,36 @@ func testReturnStatement(t *testing.T, s ast.Statement, returnValue ast.Expressi
 
 	return true
 }
+
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	expected_statements := 1
+	if len(program.Statements) != expected_statements {
+		t.Fatalf("program.Statements Does not contain %d statements. got=%d",
+			expected_statements, (program.Statements))
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("exp not *ast.Identifier. got=%T", stmt.Expression)
+	}
+	if ident.Value != "foobar" {
+		t.Errorf("ident.Value not %s. got=%s", "foobar", ident.Value)
+	}
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", "foobar",
+			ident.TokenLiteral())
+	}
+}

81e406f8d595a029383babf528e1a4edf6dd424a
Jacob Jensen <jacob.jensen@instacart.com>
2024-05-19 20:46:07 -0400

    add CallExpression test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -610,6 +610,46 @@ func TestFunctionParameterParsing(t *testing.T) {
 
 }
 
+func TestCallExpressionParsing(t *testing.T) {
+	input := `add(1, 2 * 3, 4 + 5)`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	checkParseErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+			1, len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	call, ok := stmt.Expression.(*ast.CallExpression)
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.CallExpression. got=%T",
+			stmt.Expression)
+	}
+
+	if !testIdentifier(t, call.Function, "add") {
+		return
+	}
+
+	if len(call.Arguments) != 3 {
+		t.Fatalf("function literal parameters wrong. want 3, got=%d\n",
+			len(call.Arguments))
+	}
+
+	testLiteralExpression(t, call.Arguments[0], "1")
+	testInfixExpression(t, call.Arguments[1], 2, "*", 3)
+	testInfixExpression(t, call.Arguments[2], 4, "+", 5)
+}
+
 //
 
 func TestOperatorPrecedenceParsing(t *testing.T) {
